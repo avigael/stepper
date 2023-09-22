decde325f1fdf13c8eb6b44f39742a6a7d81eb19
Gael Gomez <code@gael.cc>
2023-09-22 18:27:09 -0500

    keyboard input 3

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ try:
     motor1 = MOTOR(port=1)
     motor2 = MOTOR(port=2)
     STEP = 200
-    DELAY = 0.00005
+    DELAY = 0.0005
 
     keyboard.add_hotkey('w', lambda: motor2.turnStep(dir='L', steps=STEP, stepdelay=DELAY))
     keyboard.add_hotkey('s', lambda: motor2.turnStep(dir='R', steps=STEP, stepdelay=DELAY))
