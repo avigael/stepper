378c02c427dc16ed4fb96c336aca478295f7a7ce
Gael Gomez <code@gael.cc>
2023-09-22 18:35:15 -0500

    sshpy

---
+++ python/testssh.py
@@ -0,0 +1,7 @@
+from sshkeyboard import listen_keyboard
+
+def press(key):
+    print(f"'{key}' pressed")
+def release(key):
+    print(f"'{key}' released")
+listen_keyboard(on_press=press,on_release=release)
