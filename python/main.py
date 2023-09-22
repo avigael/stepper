a687c1eae823181fc32dbc774437919ce9a1f006
Gael Gomez <code@gael.cc>
2023-09-22 18:09:06 -0500

    attempt 2

--- python/main.py
+++ python/main.py
@@ -9,18 +9,16 @@ try:
     DELAY = 0.00005
 
     while True:
-        if (keyboard.read_key() == "w"):
+        if (keyboard.is_pressed() == "w"):
             motor2.turnStep(dir='L', steps=STEP, stepdelay=DELAY)
-        elif (keyboard.read_key() == "s"):
+        elif (keyboard.is_pressed() == "s"):
             motor2.turnStep(dir='R', steps=STEP, stepdelay=DELAY)
-        elif (keyboard.read_key() == "a"):
+        elif (keyboard.is_pressed() == "a"):
             motor1.turnStep(dir='L', steps=STEP, stepdelay=DELAY)
-        elif (keyboard.read_key() == "d"):
+        elif (keyboard.is_pressed() == "d"):
             motor1.turnStep(dir='R', steps=STEP, stepdelay=DELAY)
-        elif (keyboard.read_key() == "q"):
+        elif (keyboard.is_pressed() == "q"):
             exit()
-        motor1.stop()
-        motor2.stop()
 
 except:
     print("\nMotor stop")
