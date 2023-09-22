9eefc14fd806241fb32a8b61303055c437ea9895
Gael Gomez <code@gael.cc>
2023-09-22 18:05:52 -0500

    python keyboard

--- python/main.py
+++ python/main.py
@@ -1,27 +1,26 @@
-import RPi.GPIO as GPIO
-import time
 from MOTOR import MOTOR
+import keyboard
 
 
 try:
     motor1 = MOTOR(port=1)
     motor2 = MOTOR(port=2)
     STEP = 200
-    DELAY = 0.0005
+    DELAY = 0.00005
 
     while True:
-        print('Enter command:')
-        command = input()
-        if (command == "w"):
+        if (keyboard.read_key() == "w"):
             motor2.turnStep(dir='L', steps=STEP, stepdelay=DELAY)
-        elif (command == "s"):
+        elif (keyboard.read_key() == "s"):
             motor2.turnStep(dir='R', steps=STEP, stepdelay=DELAY)
-        elif (command == "a"):
+        elif (keyboard.read_key() == "a"):
             motor1.turnStep(dir='L', steps=STEP, stepdelay=DELAY)
-        elif (command == "d"):
+        elif (keyboard.read_key() == "d"):
             motor1.turnStep(dir='R', steps=STEP, stepdelay=DELAY)
-        elif (command == "q"):
+        elif (keyboard.read_key() == "q"):
             exit()
+        motor1.stop()
+        motor2.stop()
 
 except:
     print("\nMotor stop")
