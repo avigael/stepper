5eae08ed6fe02e1e70c0c089dabbaf218f0ddbbe
Gael Gomez <code@gael.cc>
2023-09-22 18:30:17 -0500

    testing 1.0.0

--- python/main.py
+++ python/main.py
@@ -7,13 +7,16 @@ try:
     motor2 = MOTOR(port=2)
     STEP = 200
     DELAY = 0.0005
+    # It records all the keys until escape is pressed
+    rk = keyboard.record(until ='Esc')
+    # It replay back the all keys
+    keyboard.play(rk, speed_factor = 1)
+    # keyboard.add_hotkey('w', lambda: motor2.turnStep(dir='L', steps=STEP, stepdelay=DELAY))
+    # keyboard.add_hotkey('s', lambda: motor2.turnStep(dir='R', steps=STEP, stepdelay=DELAY))
+    # keyboard.add_hotkey('a', lambda: motor1.turnStep(dir='L', steps=STEP, stepdelay=DELAY))
+    # keyboard.add_hotkey('d', lambda: motor1.turnStep(dir='R', steps=STEP, stepdelay=DELAY))
 
-    keyboard.add_hotkey('w', lambda: motor2.turnStep(dir='L', steps=STEP, stepdelay=DELAY))
-    keyboard.add_hotkey('s', lambda: motor2.turnStep(dir='R', steps=STEP, stepdelay=DELAY))
-    keyboard.add_hotkey('a', lambda: motor1.turnStep(dir='L', steps=STEP, stepdelay=DELAY))
-    keyboard.add_hotkey('d', lambda: motor1.turnStep(dir='R', steps=STEP, stepdelay=DELAY))
-
-    keyboard.wait('esc')
+    # keyboard.wait('esc')
 
 except:
     print("\nMotor stop")
